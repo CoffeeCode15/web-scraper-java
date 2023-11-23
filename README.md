67b37ff9bbaa5e01bdbb56942b89a5abba20eec1
emanuele <conte.emanuele@protonmail.com>
2023-11-23 18:10:22 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# java-web-scraper
+Simple web scraper app in Java.
+
+The taget of this app is the website: https://scrapeme.live/shop/  
+A static HTML website made to be scraped.
+
+It is just a simple paginated list of Pokemon-inspired products.  
+The goal of the Java web scraper will be to crawl the entire website and retrieve all product data.
---
+++ src/main/java/org/example/data/PokemonProduct.java
@@ -0,0 +1,50 @@
+package org.example.data;
+
+public class PokemonProduct {
+    private String url;
+    private String image;
+    private String name;
+    private String price;
+
+    // getters and setters
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "{ \"url\":\"" + url + "\", "
+                + " \"image\": \"" + image + "\", "
+                + "\"name\":\"" + name + "\", "
+                + "\"price\": \"" + price + "\" }";
+    }
+}
